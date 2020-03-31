ffe5895485634548c7c25849ff102ffdae7ee24e
Disaac1 <disaac1256@gmail.com>
2020-03-31 13:55:11 -0500

    Day 11

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1585679330",
+        "cacheTag": "1585680870",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1585678623,
+            "last_rendertime": 1585680232,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
